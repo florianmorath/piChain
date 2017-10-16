53057f086ed755e3c6ba67561b21ae53415f3d5c
Florian Morath <morathflorian@gmail.com>
2017-10-16 15:07:23 +0200

    added helper method: create block

--- PaxosProtocol.py
+++ PaxosProtocol.py
@@ -15,7 +15,7 @@ class Block:
     def __init__(self, creator_id, parent, txs):
         self.creator_id = creator_id
         self.SEQ = next(Block.new_seq)
-        self.parent = parent  # pointer to a parent block
+        self.parent = parent  # parent block
         self.txs = txs  # list of transactions of type Transaction
 
         if parent:
@@ -57,15 +57,34 @@ class Node:
     def __init__(self):
         self.id = next(Node.new_id)
         self.state = QUICK
+        self.new_txs = set()  # txs not yet in a block
+        self.head_block = GENESIS  # deepest block in the block tree (head of the blockchain)
+        self.blocks = set()  # all blocks seen by the node
+
+    # main methods
 
     def receive_message(self, message):
         """Receive a message of type Message. Return answer of type Message."""
 
     def receive_transaction(self, txn):
-        """ react on a received txn depending on"""
+        """React on a received txn depending on state"""
+        # add txn to set of new txs if not yet seen
 
     def receive_block(self, block):
-        """ react on a received block """
+        """React on a received block """
+        # add block to set of blocks seen so far
+        self.blocks.add(block)
 
+        # readjust head block if necessary
+        if self.head_block < block:
+            self.head_block = block
 
+    # helper methods
 
+    def create_block(self):
+        """Create a block containing new txs and return it."""
+        # create block
+        b = Block(self.id, self.head_block, list(self.new_txs))
+        self.new_txs.clear()
+
+        return b
